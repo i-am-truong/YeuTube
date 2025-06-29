8df64dfd97df2784126b7b61902f61c60ef4857a
Bernie Truong <tqtolympia@gmail.com>
2025-06-29 23:07:13 +0700

    feat: add user page and banner upload feature for project

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -9,72 +9,104 @@ import { eq, and } from "drizzle-orm";
 const f = createUploadthing();
 
 export const ourFileRouter = {
+  bannerUploader: f({
+    image: {
+      maxFileSize: "4MB",
+      maxFileCount: 1,
+    },
+  })
+    .middleware(async ({}) => {
+      const { userId: clerkUserId } = await auth();
 
-    thumbnailUploader: f({
-        image: {
-            maxFileSize: "4MB",
-            maxFileCount: 1,
-        },
+      if (!clerkUserId) throw new UploadThingError("Unauthorized");
+      const [existingUser] = await db
+        .select()
+        .from(users)
+        .where(eq(users.clerkId, clerkUserId));
+      if (!existingUser) throw new UploadThingError("Unauthorized");
+
+      if (existingUser.bannerKey) {
+        const utapi = new UTApi();
+        await utapi.deleteFiles(existingUser.bannerKey);
+        await db
+          .update(users)
+          .set({ bannerKey: null, bannerUrl: null })
+          .where(eq(users.id, existingUser.id));
+      }
+
+      return { userId: existingUser.id };
     })
-        .input(z.object({
-            videoId: z.string().uuid()
-        }))
-        .middleware(async ({ input }) => {
-
-            const { userId: clerkUserId } = await auth();
-
-
-            if (!clerkUserId) throw new UploadThingError("Unauthorized");
-            const [user] = await db
-                .select()
-                .from(users)
-                .where(eq(users.clerkId, clerkUserId))
-            if (!user) throw new UploadThingError("Unauthorized");
-
-            const [existingVideo] = await db
-                .select(
-                    {
-                        thumbnailKey: videos.thumbnailKey
-                    }
-                )
-                .from(videos)
-                .where(and(
-                    eq(videos.id, input.videoId),
-                    eq(videos.userId, user.id)
-                ))
-
-            if (!existingVideo) {
-                throw new UploadThingError("Not found!")
-            }
-
-            if (existingVideo.thumbnailKey) {
-                const utapi = new UTApi()
-                await utapi.deleteFiles(existingVideo.thumbnailKey)
-                await db
-                    .update(videos)
-                    .set({ thumbnailKey: null, thumbnailUrl: null })
-                    .where(and(
-                        eq(videos.id, input.videoId),
-                        eq(videos.userId, user.id)
-                    ))
-            }
-
-            return { user, ...input };
+    .onUploadComplete(async ({ metadata, file }) => {
+      await db
+        .update(users)
+        .set({
+          bannerUrl: file.ufsUrl,
+          bannerKey: file.key,
         })
-        .onUploadComplete(async ({ metadata, file }) => {
-            await db
-                .update(videos)
-                .set({
-                    thumbnailUrl: file.ufsUrl,
-                    thumbnailKey: file.key
-                })
-                .where(and(
-                    eq(videos.id, metadata.videoId),
-                    eq(videos.userId, metadata.user.id)
-                ))
+        .where(eq(users.id, metadata.userId));
 
-            return { uploadedBy: metadata.user.id };
-        }),
+      return { uploadedBy: metadata.userId };
+    }),
+
+  thumbnailUploader: f({
+    image: {
+      maxFileSize: "4MB",
+      maxFileCount: 1,
+    },
+  })
+    .input(
+      z.object({
+        videoId: z.string().uuid(),
+      })
+    )
+    .middleware(async ({ input }) => {
+      const { userId: clerkUserId } = await auth();
+
+      if (!clerkUserId) throw new UploadThingError("Unauthorized");
+      const [user] = await db
+        .select()
+        .from(users)
+        .where(eq(users.clerkId, clerkUserId));
+      if (!user) throw new UploadThingError("Unauthorized");
+
+      const [existingVideo] = await db
+        .select({
+          thumbnailKey: videos.thumbnailKey,
+        })
+        .from(videos)
+        .where(and(eq(videos.id, input.videoId), eq(videos.userId, user.id)));
+
+      if (!existingVideo) {
+        throw new UploadThingError("Not found!");
+      }
+
+      if (existingVideo.thumbnailKey) {
+        const utapi = new UTApi();
+        await utapi.deleteFiles(existingVideo.thumbnailKey);
+        await db
+          .update(videos)
+          .set({ thumbnailKey: null, thumbnailUrl: null })
+          .where(and(eq(videos.id, input.videoId), eq(videos.userId, user.id)));
+      }
+
+      return { user, ...input };
+    })
+    .onUploadComplete(async ({ metadata, file }) => {
+      await db
+        .update(videos)
+        .set({
+          thumbnailUrl: file.ufsUrl,
+          thumbnailKey: file.key,
+        })
+        .where(
+          and(
+            eq(videos.id, metadata.videoId),
+            eq(videos.userId, metadata.user.id)
+          )
+        );
+
+      return { uploadedBy: metadata.user.id };
+    }),
 } satisfies FileRouter;
 
 export type OurFileRouter = typeof ourFileRouter;
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,27 +1,29 @@
-import { categoriesRouter } from '@/modules/categories/server/procedures';
-import { createTRPCRouter } from '../init';
-import { studioRouter } from '@/modules/studio/server/procedures';
-import { videosRouter } from '@/modules/videos/server/procedures';
-import { videoViewsRouter } from '@/modules/video-views/server/procedures';
-import { videoReactionRouter } from '@/modules/video-reactions/server/procedures';
-import { subscriptionsRouter } from '@/modules/subscriptions/server/procedures';
-import { commentsRouter } from '@/modules/comments/server/procedures';
-import { commentReactionRouter } from '@/modules/comment-reactions/server/procedures';
-import { suggestionsRouter } from '@/modules/suggestions/server/procedures';
-import { searchRouter } from '@/modules/search/server/procedures';
-import { playlistsRouter } from '@/modules/playlist/server/procedures';
+import { categoriesRouter } from "@/modules/categories/server/procedures";
+import { createTRPCRouter } from "../init";
+import { studioRouter } from "@/modules/studio/server/procedures";
+import { videosRouter } from "@/modules/videos/server/procedures";
+import { videoViewsRouter } from "@/modules/video-views/server/procedures";
+import { videoReactionRouter } from "@/modules/video-reactions/server/procedures";
+import { subscriptionsRouter } from "@/modules/subscriptions/server/procedures";
+import { commentsRouter } from "@/modules/comments/server/procedures";
+import { commentReactionRouter } from "@/modules/comment-reactions/server/procedures";
+import { suggestionsRouter } from "@/modules/suggestions/server/procedures";
+import { searchRouter } from "@/modules/search/server/procedures";
+import { playlistsRouter } from "@/modules/playlist/server/procedures";
+import { usersRouter } from "@/modules/users/server/procedures";
 export const appRouter = createTRPCRouter({
-    studio: studioRouter,
-    videos: videosRouter,
-    search: searchRouter,
-    comments: commentsRouter,
-    playlists: playlistsRouter,
-    categories: categoriesRouter,
-    videoViews: videoViewsRouter,
-    subscriptions: subscriptionsRouter,
-    videoReactions: videoReactionRouter,
-    suggestions: suggestionsRouter,
-    commentReactions: commentReactionRouter
+  studio: studioRouter,
+  videos: videosRouter,
+  search: searchRouter,
+  users: usersRouter,
+  comments: commentsRouter,
+  playlists: playlistsRouter,
+  categories: categoriesRouter,
+  videoViews: videoViewsRouter,
+  subscriptions: subscriptionsRouter,
+  videoReactions: videoReactionRouter,
+  suggestions: suggestionsRouter,
+  commentReactions: commentReactionRouter,
 });
 // export type definition of API
 export type AppRouter = typeof appRouter;
